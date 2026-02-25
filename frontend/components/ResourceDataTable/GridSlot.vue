5a924284e9063a655e4d9674ae29c6cb17af72e6
Jeff Surrett <jeff@surrett.org>
2026-02-25 08:38:46 -0500

    Update the ResourceTable GridSlot for card view

--- frontend/components/ResourceDataTable/GridSlot.vue
+++ frontend/components/ResourceDataTable/GridSlot.vue
@@ -1,26 +1,40 @@
 <template v-slot:item="props">
   <div
-    class="q-pa-xs col-xs-12 col-sm-6 col-md-4 col-lg-3 grid-style-transition"
+    class="q-pa-md col-xs-12 col-sm-6 col-md-4 col-lg-3 grid-style-transition"
     :style="props.selected ? 'transform: scale(0.95);' : ''"
   >
-    <q-card :class="props.selected ? 'bg-grey-2' : ''">
-      <!-- <q-card-section>
-        <q-checkbox dense v-model="props.selected" :label="props.row.name" />
-      </q-card-section> -->
-      <!-- <q-separator /> -->
-      <q-list dense>
-        <q-item v-for="col in props.cols" :key="col.name">
-          <div class="q-table__grid-item-row">
-            <div class="q-table__grid-item-title">{{ col.label }}</div>
-            <div class="q-table__grid-item-value" >
-              <row-actions v-if="col.name === 'actions'" :resourceName="resourceName" :item="props.row" />
-              <slot v-else :column="col" :row="props.row">
-                {{ col.value }}
-              </slot>
-            </div>
+    <q-card
+      :class="props.selected ? 'bg-primary text-white shadow-8' : 'bg-grey-1 shadow-2'"
+      rounded
+    >
+      <q-card-section class="q-pb-xs">
+        <div class="row justify-between items-start">
+          <div class="col"></div>
+          <div v-if="props.cols.some(col => col.name === 'actions')" class="col-auto q-ml-md">
+            <row-actions
+              :resourceName="resourceName"
+              :item="props.row"
+            />
           </div>
-        </q-item>
-      </q-list>
+        </div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-sm">
+        <div
+          v-for="(col, index) in props.cols.filter(c => c.name !== 'actions')"
+          :key="col.name"
+          :class="index < props.cols.length - 2 ? 'q-mb-lg' : ''"
+        >
+          <div class="text-caption text-weight-bold" :class="props.selected ? 'text-white' : 'text-grey-7'">
+            {{ col.label }}
+          </div>
+          <div class="text-body2 text-weight-medium q-mt-xs">
+            <slot :column="col" :row="props.row">
+              {{ col.value }}
+            </slot>
+          </div>
+        </div>
+      </q-card-section>
     </q-card>
   </div>
 </template>
